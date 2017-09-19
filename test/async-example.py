67a6b26b58988669d38602f75973e138020f1f3d
SuperHighMan <465475547@qq.com>
2017-09-19 22:23:06 +0800

    async-example

---
+++ test/async-example.py
@@ -0,0 +1,18 @@
+from proj.async import AsyncCheck
+import datetime
+
+if __name__ == '__main__':
+    args = [['192.168.92.133'], 'uptime']
+    begin = datetime.datetime.now()
+    check = AsyncCheck(args=args, interval=3, times=2)
+    check.run()
+    print(check.timeout_result())
+    end = datetime.datetime.now()
+    print(end - begin)
+
+    begin = datetime.datetime.now()
+    check = AsyncCheck(args=args)
+    check.run()
+    print(check.timeout_result())
+    end = datetime.datetime.now()
+    print(end - begin)
